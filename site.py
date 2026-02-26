6504a858d70d18ae98b1b10a67eebbe54f6d4dc0
mo <mo@mos-MacBook-Pro-2.local>
2026-02-25 22:56:59 -0500

    working version3 test

--- site.py
+++ site.py
@@ -11,7 +11,8 @@ data = yf.download(coin, period=period, progress=False)
 data = data.dropna()
 #btc = yf.download("BTC-USD", period="6mo", progress=False)
 #st.line_chart(btc["Close"])
-events={"Growth":[],"Drop":[{"date":"","title":"Ms sells btc","description":"jhebcheb"},{"date":"2022-11-09","title":"FTX Collapse","description":"ms promised not to sell etc, but they did"}]}
+events={"Growth":[],"Drop":[{"dec 2022":"","title":"Ms sells btc","description":"promised not to sell it but sold it"},
+                            {"date":"2022-11-09","title":"FTX Collapse","withdrawals around the world":"ms promised not to sell etc, but they did"}]}
 eventlist=events[mode]
 if eventlist:
     eventtitle=[e["title"] for e in eventlist]
