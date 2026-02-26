297439188100754dbf7bb646414ac364ff7c92fd
mo <mo@mos-MacBook-Pro-2.local>
2026-02-26 03:00:04 -0500

    buttons test

--- site.py
+++ site.py
@@ -12,7 +12,7 @@ data = yf.download(coin, period=period, progress=False)
 data = data.dropna()
 #btc = yf.download("BTC-USD", period="6mo", progress=False)
 #st.line_chart(btc["Close"])
-events={"Growth":[],"Drop":[{"2022-12-01":"","title":"Ms sells btc","description":"promised not to sell it but sold it"},
+events={"Growth":[],"Drop":[{"date":"2022-12-01","title":"Ms sells btc","description":"promised not to sell it but sold it"},
                             {"date":"2022-11-09","title":"FTX Collapse","withdrawals around the world":"ms promised not to sell etc, but they did"}]}
 eventlist=events[mode]
 if eventlist:
@@ -36,4 +36,4 @@ else:
     st.warning("No data available")
 
 
-#st.write("1.0")
+#st.write("1.1")
