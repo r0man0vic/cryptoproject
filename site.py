99bbb2ac9fb07ecd8e38504a2b1474d5739d1c4f
mo <mo@mos-MacBook-Pro-2.local>
2026-04-05 17:39:37 -0400

    covid19

--- site.py
+++ site.py
@@ -14,7 +14,7 @@ data = data.dropna()
 #st.line_chart(btc["Close"])
 events={"Growth":[],
         "Drop":[{"date":"2022-12-01","title":"Ms sells btc","description":"promised not to sell it but sold it","window":20},
-                {"date":"2011-06-01","title":"Mt.gox hack","withdrawals around the world":"ms promised not to sell etc, but they did","window":30}]}
+                {"date":"2020-03-01","title":"Covid-19","withdrawals around the world":"ms promised not to sell etc, but they did","window":30}]}
                             #{"date":"20-11-09","title":"FTX Collapse","withdrawals around the world":"ms promised not to sell etc, but they did"}]}
 eventlist=events[mode]
 if eventlist:
