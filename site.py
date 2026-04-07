9affb9faa03b2ec8921a82ac8b61c0866b0cbf7d
mo <mo@mos-MacBook-Pro-2.local>
2026-04-07 14:09:50 -0400

    хронология события восстановил

--- site.py
+++ site.py
@@ -16,7 +16,7 @@ events={"Growth":[
                   {"date":"2020-12-10","title":"digital gold","description":"gazz","window":90}],
         "Drop":[
                 {"date":"2018-04-16","title":"Bear market in 2017","description":"The 2017 bull run created massive speculation. When the bubble popped, the entire crypto market entered a deep bear cycle.","window":120},
-                {"date":"2020-03-10","title":"Covid-19 crypto crisis","description":"global pandemic","window":30},
+                {"date":"2020-02-20","title":"Covid-19 crypto crisis","description":"global pandemic","window":30},
                 {"date":"2021-05-15","title":"China mining ban + Elon Musk tweets ","description":"China’s mining crackdown and macro uncertainty triggered a sharp correction after a historic bull run. Elon Musk's tweets regarding energy concerns","window":50},
                 {"date":"2021-12-18","title":"Federal Reserve tightening","description":"Bitcoin declined as the Federal Reserve signaled tighter monetary policy, reducing liquidity and pushing investors away from risk assets.","window":35},
                 {"date":"2022-05-4","title":"Terra crash","description":"Terra collapsed","window":45},
